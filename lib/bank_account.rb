2dd5c221c4a71f7492572b3f2df2e133fe24162f
Samuel Cornet <samuel@noukod.com>
2018-10-10 13:00:10 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
+  attr_accessor :name
+  attr_writer :balance
+
+  def initialize(name)
+     balance= 1000
+    @name = name
+
+    transactions = []
+    add_transaction(0)
+  end
+
+  def display_balance=(balance)
+    @balance = 1000
+  end
+
+  def deposit(amount)
+    (@balance = @balance + amount) if (amount > 0)
+  end
+
+  def display_balance
+    balance = 0
+    @transactions.each do |transaction|
+      balance += transaction
+    end
+      return balance
+    end
+
+
+  def valid?
+
+  end
+  def close_account
+
+  end
 
 end
