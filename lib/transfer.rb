0f9a51297cd601f785a0b365cfd6a29a6e6cb3f3
Samuel Cornet <samuel@noukod.com>
2018-10-10 13:44:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,33 @@
 class Transfer
-  attr_accessor :transfer, :sender, :receiver
+  attr_accessor :transfer_amount, :sender, :receiver
 
-  def initialize(transfer_amount)
-    @transfer = transfer
+  def initialize(transfer_amount, sender, receiver)
+    @transfer_amount = transfer
     @sender = sender
     @receiver = receiver
     @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
   end
 end
