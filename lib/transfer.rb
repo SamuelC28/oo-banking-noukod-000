0b6e653fb919b83fe816c684d9443c448ad72a17
Samuel Cornet <samuel@noukod.com>
2018-10-10 13:38:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,10 @@
 class Transfer
-  attr_accessor :transfer
+  attr_accessor :transfer, :sender, :receiver
 
-  def initialize
+  def initialize(transfer_amount)
     @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
   end
 end
